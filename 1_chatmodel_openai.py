26b807c5fe9b5e5ee4b5faed8b5550b35644b78a
youngdumb135-create <youngdumb135@gmail.com>
2025-09-18 15:37:18 +0530

    Add files via upload

---
+++ 1_chatmodel_openai.py
@@ -0,0 +1,10 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatOpenAI(model="gpt-4", temperature= 0.1, max_completion_tokens=10)
+
+result = model.invoke("What is the capital of india?")
+
+print(result.content)
